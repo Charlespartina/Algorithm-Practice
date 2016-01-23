51bbb58cfc6d37dbc0acc0d0d67f42f18685074f
Charlespartina <tangziyi001@gmail.com>
2016-01-23 10:15:27 +0800

    Update

--- LeetCode/Implementation/BST.cpp
+++ LeetCode/Implementation/BST.cpp
@@ -11,37 +11,77 @@ struct BST{
 	Node* root;
 	int sz;
 	BST() : sz(0),root(NULL){}
-
 	void insert(int val){
-		insert(val,root);
+		root = insert(val,root);
 	}
-	void insert(int val, Node* now){
-		if(now == NULL) now = new Node(val);
+	Node* insert(int val, Node* now){
+		if(now == NULL){
+			now = new Node(val);
+			sz++;
+		}
 		else if (val <= now->val){
-			insert(val,now->left);
+			now->left = insert(val,now->left);
 		}
 		else{
-			insert(val,now->right);
+			now->right = insert(val,now->right);
+		}
+		return now;
+	}
+	void remove(int val){
+		if(root != NULL){
+			remove(val,root);
 		}
 	}
-	void remove(int val, Node* now){
+	Node* remove(int val, Node* now){
 		if(now == NULL){
-			cout << "NOT FOUND" << endl;
+			return NULL;
 		}
 		else if(now->val < val){
-			remove(val, now->left);
+			now->left = remove(val, now->left);
 		}
 		else if(now->val > val){
-			remove(val, now->right);
+			now->right = remove(val, now->right);
 		}
 		else{
-			
+			now = removeNode(now);
 		}
+		return now;
+	}
+	Node* removeNode(Node* now){
+		if(now->left == NULL){
+			sz--;
+			Node* tmp = now->right;
+			delete(now);
+			return tmp;
+		}
+		if(now->right == NULL){
+			sz--;
+			Node* tmp = now->left;
+			delete(now);
+			return tmp;
+		} 
+		else{
+			int tmp = getPre(now->left);
+			now->val = tmp;
+			remove(tmp, now->left);
+			return now;
+		}
+
+	}
+	int getPre(Node* now){
+		if(now->right != NULL) now = now->right;
+		return now->val;
+	}
+	int size(){
+		return sz;
 	}
 };
 
 int main(){
-
-
+	BST* mybst = new BST();
+	for(int i = 0; i < 100; i++)
+		mybst->insert(i);
+	mybst->remove(10);
+	cout << mybst->size() << endl;
 	return 0;
 }
