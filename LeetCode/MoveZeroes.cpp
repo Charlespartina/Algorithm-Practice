f7fca637e33b59581540f6aa655cd2fc2ff3c7c9
Charlespartina <tangziyi001@gmail.com>
2016-02-03 22:48:21 -0500

    Update

--- LeetCode/MoveZeroes.cpp
+++ LeetCode/MoveZeroes.cpp
@@ -1,16 +1,15 @@
-// Use vector.erase()
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int sz = nums.size();
-        int cont = 0;
-        for(int i = 0; i < sz && cont < sz; i++){
-            cont++;
-            if(nums[i] == 0){
-                nums.erase(nums.begin()+i);
-                i--;
-                nums.push_back(0);
-            }
+        if(sz == 0) return ;
+        int p = 0;
+        for(int i = 0; i < sz; i++){
+            if(nums[i] != 0) nums[p++] = nums[i];
         }
+        while(p < sz){
+            nums[p++] = 0;
+        }
+        return ;
     }
 };
